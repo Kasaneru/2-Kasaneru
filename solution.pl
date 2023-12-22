f97a3b1ded37e3eb33f5e82bed4e0e930d0d7ee9
Kasaneru <kasaneru.san@gmail.com>
2023-12-22 20:39:02 +0300

    lab2

--- solution.pl
+++ solution.pl
@@ -1,1 +1,11 @@
 % Place your solution here
+
+owns('Воронов', X).
+ismarried('Голубев', 0).
+ismarried('Канарейкин', 0).
+ismarried('Грачев', 1).
+ismarried('Воронов', 1).
+ismarried('Скворцов', 1).
+ismarried('Попугаев', 1).
+ismarried('Чайкин', 1).
+darkfur().
